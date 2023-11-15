3048f9c147506b94518de8ffb85e41c9eda4b4de
atiks88 <141978852+atiks88@users.noreply.github.com>
2023-11-15 18:40:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function shortcut(s1, s2) {
   // your code here
+  let x = '';
+  if (s1.length > 0 && s2.length > 0) {
+    x = s1[0] + s2[0];
+  } else {
+    return x;
+  }
+  return x;
 }
-
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
